773de369fd222178981f8bf974f199a312fe7bfd
Pariya <pariyaafsharpour79@gmail.com>
2024-06-04 18:00:48 +0330

    fixed priority column

--- models/task.py
+++ models/task.py
@@ -10,7 +10,7 @@ class TaskModel(BaseModel):
     name = db.Column(db.String(60), unique=False, nullable=False)
     description = db.Column(db.String(120), unique=False, nullable=True)
     due_date = db.Column(db.Boolean, unique=False, nullable=True)
-    priority = db.Column(db.Column(db.Integer, unique=False, default=1, nullable=False))
+    priority = db.Column(db.Integer, unique=False, default=1, nullable=False)
     is_completed = db.Column(db.Boolean, default=False, nullable=False)
     category = db.Column(db.String(50), nullable=True)
 
