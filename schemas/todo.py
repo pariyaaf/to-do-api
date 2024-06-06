acd5ce1fcd97849a789dadbc0b2c0ae27603b597
Pariya <pariyaafsharpour79@gmail.com>
2024-06-06 17:37:16 +0330

    fixed bug

--- schemas/todo.py
+++ schemas/todo.py
@@ -6,7 +6,7 @@ class PlainListSchema(BaseSchema):
     id = fields.Int(dump_only=True)
     name = fields.String(required=True)
     color = fields.String()
-    is_active = fields.boolean(required=True)
+    is_active = fields.Boolean(required=True)
 
 
 class PlainTaskSchema(BaseSchema):
@@ -15,17 +15,19 @@ class PlainTaskSchema(BaseSchema):
     description = fields.String()
     due_date = fields.DateTime()
     priority = fields.Int()
-    is_completed = fields.boolean(required=True)
+    is_completed = fields.Boolean(required=True)
     category = fields.Str()
 
 
 class ListSchema(PlainListSchema):
-    user_id = fields.Int(required=True, load_only=True)
+    user_id = fields.Int(load_only=True)
     tasks = fields.List(fields.Nested(PlainTaskSchema()), dump_only=True)
 
 
 
 class TaskSchema(PlainTaskSchema):
+    list_id = fields.Int(required=True, load_only=True)
+
     list = fields.Nested(PlainListSchema(), dump_only=True)
 
 
@@ -35,11 +37,11 @@ class UpateTaskSchema(Schema):
     description = fields.String()
     due_date = fields.DateTime()
     priority = fields.Int()
-    is_completed = fields.boolean()
+    is_completed = fields.Boolean()
     category = fields.Str()
 
 
 class UpdateListSchema(Schema):
     name = fields.String()
     color = fields.String()
-    is_active = fields.boolean()
+    is_active = fields.Boolean()
