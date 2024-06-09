8878b0a880dd5e863a80980682533560e07e2d3b
Pariya <pariyaafsharpour79@gmail.com>
2024-06-09 13:18:21 +0330

    fix task and list shcema

--- schemas/todo.py
+++ schemas/todo.py
@@ -13,26 +13,24 @@ class PlainTaskSchema(BaseSchema):
     id = fields.Int(dump_only=True)
     name = fields.String(required=True)
     description = fields.String()
-    due_date = fields.DateTime()
+    due_date = fields.Date()
     priority = fields.Int()
-    is_completed = fields.Boolean(required=True)
+    is_completed = fields.Boolean()
     category = fields.Str()
 
 
 class ListSchema(PlainListSchema):
-    user_id = fields.Int(load_only=True)
+    user_id = fields.Int()
     tasks = fields.List(fields.Nested(PlainTaskSchema()), dump_only=True)
 
 
-
 class TaskSchema(PlainTaskSchema):
-    list_id = fields.Int(required=True, load_only=True)
+    list_id = fields.Int(load_only=True)
 
     list = fields.Nested(PlainListSchema(), dump_only=True)
 
-
-
-class UpateTaskSchema(Schema):
+class UpdateTaskSchema(Schema):
+    list_id = fields.Int()
     name = fields.String()
     description = fields.String()
     due_date = fields.DateTime()
