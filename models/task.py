7ba71fae445be7021f83d2152a68937dc62222a0
Pariya <pariyaafsharpour79@gmail.com>
2024-06-09 13:20:02 +0330

    fix due_date and list

--- models/task.py
+++ models/task.py
@@ -9,11 +9,13 @@ class TaskModel(BaseModel):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(60), unique=False, nullable=False)
     description = db.Column(db.String(120), unique=False, nullable=True)
-    due_date = db.Column(db.Boolean, unique=False, nullable=True)
+    due_date = db.Column(db.Date, unique=False, nullable=True)
     priority = db.Column(db.Integer, unique=False, default=1, nullable=False)
     is_completed = db.Column(db.Boolean, default=False, nullable=False)
     category = db.Column(db.String(50), nullable=True)
 
-    list = db.relationship("UserModel", back_populates="tasks")
+    #lists
+    list_id = db.Column(db.Integer, db.ForeignKey('lists.id'), nullable=True)
+    list = db.relationship("ListModel", back_populates="tasks")
 
 
